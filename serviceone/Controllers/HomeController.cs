98540e1b56142aeadd1f5c23f8c52eaa2fd4ba77
slava <slava.yandex.ru23@mail.ru>
2022-12-27 13:04:51 +0300

    First

---
+++ serviceone/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System.Net.Http.Headers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace serviceone.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class HomeController : ControllerBase
+{
+    private HttpClient _client;
+
+    public HomeController()
+    {
+        _client = new HttpClient();
+        _client.BaseAddress = new Uri(Environment.GetEnvironmentVariable("WeatherService")!);
+        _client.DefaultRequestHeaders.Accept.Add(
+            new MediaTypeWithQualityHeaderValue("application/json"));
+    }
+
+    [HttpGet("test")]
+    public IActionResult Test()
+    {
+        return Ok("Single works!");
+    }
+    
+    
+    [HttpGet("network")]
+    public IActionResult NetworkRequest()
+    {
+        var response = _client.GetAsync("WeatherForecast").Result;
+        return Ok(response.Content.ReadAsStringAsync().Result);
+    }
+}
